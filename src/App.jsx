5c20818fa2a2ff49ae7929eab3a7e9d24d155ab9
Correa Alejandro <correa92.dev@gmail.com>
2023-05-06 19:55:45 -0300

    se reestructura carpetas

--- src/App.jsx
+++ src/App.jsx
@@ -15,8 +15,9 @@ import ProductsContainer from "./components/ProductsContainer/ProductsContainer"
 import NewProduct from "./components/NewProduct/NewProduct";
 import FormEditProduct from "./components/FormEditProduct/FormEditProduct";
 import Aos from "aos";
-import NavBar from "./components/Header/NavBar/NavBar";
+import NavBar from "./components/Main/NavBar/NavBar";
 import InicioContainer from "./components/Main/InicioContainer/InicioContainer";
+
 function App() {
   Aos.init();
 
