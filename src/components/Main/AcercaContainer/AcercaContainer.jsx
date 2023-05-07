f8359efc0ab50a5040209058b368298cbcd3bf0e
luchopesce <luchopesce96@gmail.com>
2023-05-07 00:00:11 -0300

    acerca

--- src/components/Main/AcercaContainer/AcercaContainer.jsx
+++ src/components/Main/AcercaContainer/AcercaContainer.jsx
@@ -6,10 +6,10 @@ const AcercaContainer = () => {
     <div className="container_acerca">
       <div className="acerca_items-container">
         <div className="acerca_items">
-          <div className="item_acerca img">
+          <div className="item_acerca">
             <img src="../../src/img/imagen-ejemplo.jpg" alt="" />
           </div>
-          <div className="item_acerca text">Lorem ipsum dolor sit amet.</div>
+          <div className="item_acerca">Lorem</div>
         </div>
       </div>
     </div>
